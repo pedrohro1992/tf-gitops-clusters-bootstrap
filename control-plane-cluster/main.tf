28afdbdfbf10c354aa2045e62ecd69d96ca0ca81
pramos <pedrooramos@protonmail.com>
2026-02-01 16:12:20 -0300

    Adiciona boostrap crossplane

--- control-plane-cluster/main.tf
+++ control-plane-cluster/main.tf
@@ -10,3 +10,12 @@ module "argocd" {
 
   depends_on = [module.control_plane_cluster]
 }
+
+module "crossplane_bootstrap" {
+  source = "../tf-module-crossplane-app"
+
+  git_repo_url = "http://gitea-server:3000/cacetinho-sa-cloudnative/gitops-platform-config.git" # Ajuste para seu repo
+
+  # Garante que o ArgoCD já existe antes de tentar criar a Application
+  depends_on = [module.argocd]
+}
--- control-plane-cluster/providers.tf
+++ control-plane-cluster/providers.tf
@@ -8,3 +8,10 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.control_plane_cluster.cluster_ca_certificate)
   }
 }
+
+provider "kubernetes" {
+    host                   = module.control_plane_cluster.endpoint
+    client_certificate     = base64decode(module.control_plane_cluster.client_certificate)
+    client_key             = base64decode(module.control_plane_cluster.client_key)
+    cluster_ca_certificate = base64decode(module.control_plane_cluster.cluster_ca_certificate)
+}
