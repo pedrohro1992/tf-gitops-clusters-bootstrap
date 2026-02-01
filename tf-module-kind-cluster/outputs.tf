cd4e02224e96190d25584b6cdec5a360a1eebc3d
pramos <pedrooramos@protonmail.com>
2026-02-01 14:05:44 -0300

    First Commit

--- tf-module-kind-cluster/outputs.tf
+++ tf-module-kind-cluster/outputs.tf
@@ -6,3 +6,17 @@ output "kubeconfig" {
 output "endpoint" {
   value = kind_cluster.this.endpoint
 }
+
+output "client_certificate" {
+  value = kind_cluster.this.client_certificate
+}
+
+output "client_key" {
+  value = kind_cluster.this.client_key
+}
+
+output "cluster_ca_certificate" {
+  value = kind_cluster.this.cluster_ca_certificate
+}
+
+
